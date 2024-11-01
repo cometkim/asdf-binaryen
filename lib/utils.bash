03c865a180e98aad050b43f56f05d034cf8d1d3b
Hyeseong Kim <hey@hyeseong.kim>
2024-11-02 05:16:09 +0900

    fix version format

--- lib/utils.bash
+++ lib/utils.bash
@@ -10,12 +10,11 @@ if [ -n "${GITHUB_API_TOKEN:-}" ]; then
   curl_opts=("${curl_opts[@]}" -H "Authorization: token $GITHUB_API_TOKEN")
 fi
 
-# stolen from https://github.com/rbenv/ruby-build/pull/631/files#diff-fdcfb8a18714b33b07529b7d02b54f1dR942
 function sort_versions() {
   sed 's/version_//' |
-  sed 'h; s/[+-]/./g; s/.p\([[:digit:]]\)/.z\1/; s/$/.z/; G; s/\n/ /' |
-    LC_ALL=C sort -t. -k 1,1 -k 2,2n -k 3,3n -k 4,4n -k 5,5n | awk '{print $2}'
-  sed 's/^/version_/'
+    awk -F '_' '{printf "%s %s\n", $1, ($2 ? $2 : "")}' |
+    sort -k1,1n -k2,2 |
+    awk '{print "version_" $1 ($2 ? "_" $2 : "")}'
 }
 
 function list_github_releases() {
